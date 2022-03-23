9485a718bf5ba83212cbc021acd35faa97b296dd
Quyen Pham <hoangquyen174@gmail.com>
2022-03-23 10:29:27 -0500

    add push and unshift

--- dsa-arrays-linked-lists/linked-list.js
+++ dsa-arrays-linked-lists/linked-list.js
@@ -21,13 +21,34 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
+    let newNode = new Node(val);
 
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+    let newNode = new Node(val);
 
+    if(this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    if(this.length === 0) this.tail = this.head;
+
+    this.length += 1;
   }
 
   /** pop(): return & remove last item. */
